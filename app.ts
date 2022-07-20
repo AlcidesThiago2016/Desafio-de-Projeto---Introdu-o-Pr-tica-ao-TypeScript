d8540c9613b4ed0a48934748b56e2b568a06c0ad
alcid <alcides_thiago@hotmail.com>
2022-07-20 14:18:25 -0300

    Update Testes

--- app.ts
+++ app.ts
@@ -1,22 +1,27 @@
-let button = document.getElementById('button');
-let input1 = document.getElementById('input1') as HTMLInputElement;
-let input2 = document.getElementById('input2') as HTMLInputElement;
+let anyEstaDeVolta : any;
 
-function adicionarNumero(numero1: number, numero2: number, devePrintar: boolean, frase: string){
-    let resultado = numero1 + numero2
-    if(devePrintar){
-        console.log(frase + resultado)
-    }
-    return numero1 + numero2;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = 'teste';
+anyEstaDeVolta = 5;
+
+let stringTeste: string = 'verificar';
+stringTeste = anyEstaDeVolta;
+
+let unknownValor: unknown;
+unknownValor = 3;
+unknownValor = 'opa';
+unknownValor = true;
+unknownValor = 'vai sim'
+
+let stringTeste2: string  = 'agora vai';
+
+
+if(typeof unknownValor === 'string'){
+    stringTeste2 = unknownValor; 
 }
 
-let devePrintar = true;
-let frase = 'O valor é: ';
-
-if (button){
-    button.addEventListener('click', () => {
-        if(input1 && input2){
-            console.log(adicionarNumero(Number(input1.value), Number(input2.value), devePrintar, frase));
-        }
-    })
+function jogaErro(erro: string, codigo : number){
+    throw {error: erro, code: codigo}
 }
+
+jogaErro('deu erro', 500)
